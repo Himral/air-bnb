65822533356d7452f93cce4d0f18fdf465d0d98e
Himral <himralgarg@gmail.com>
2023-06-01 16:28:35 +0530

    first three sections created

---
+++ src/components/collage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Collage(){
+    return(
+        <div className='collage'>
+            <img src = "collage.png"></img>
+        </div>
+    );
+}
---
+++ src/components/text.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Text(){
+    return(
+        <div className='text'>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+</p>
+        </div>
+    );
+}
